9954d6a7171a62dfc8acbbc99060ecc37210f17b
GJuteau <godefroy.juteau.1@ens.etsmtl.ca>
2021-11-14 14:34:18 -0500

    dialog progression

--- CoCoJam2021/Assets/Scripts/DialogueController.cs
+++ CoCoJam2021/Assets/Scripts/DialogueController.cs
@@ -6,51 +6,55 @@ using TMPro;
 public class DialogueController : MonoBehaviour
 {
     public TextMeshProUGUI dialogueText;
-    public string[] sentences;
-    private int index = 0;
-    private bool nextText = true;
+    [SerializeField]
+    public List<DialogLine> sentences;
 
-    [SerializeField] private float dialogueSpeed;
-    [SerializeField] private Animator dialogueAnimator;
-    private bool startDialogue = true;
+    [SerializeField]
+    private float dialogueSpeed = 0.1f;
+    [SerializeField]
+    private float dialogueTimer = 0.5f;
+
+    [SerializeField]
+    private Animator dialogueAnimator;
+    [SerializeField]
+    private SpriteRenderer currentDialogBox;
+    [SerializeField]
+    private Sprite catClosed;
+    [SerializeField]
+    private Sprite catOpened;
+    [SerializeField]
+    private Sprite girlClosed;
+    [SerializeField]
+    private Sprite girlOpened;
+    [SerializeField]
+    private float animationDelay;
 
 
     // Update is called once per frame
     void Update()
     {
-        if(Input.GetKeyDown(KeyCode.Space))
-        {
-            if (startDialogue)
-            {
-                dialogueAnimator.SetTrigger("Enter");
-                startDialogue = false;
-            }
-            else
-            {
-                if (nextText)
-                {
-                    nextText = false;
-                    NextSentence();
-                }
-                
-            }
-        }
+        // Make sure to follow the camera
+        float camPos = Camera.main.transform.position.x;
+        if(transform.position.x != camPos)
+		{
+            transform.position = new Vector3(camPos, transform.position.y, transform.position.z);
+		}
     }
 
     IEnumerator WriteSentence()
     {
-        foreach(char character in sentences[index].ToCharArray())
+        foreach(char character in sentences[0].line.ToCharArray())
         {
             dialogueText.text += character;
             yield return new WaitForSeconds(dialogueSpeed);
         }
-        index++;
-        nextText = true;
+        sentences.RemoveAt(0);
+        yield return new WaitForSeconds(dialogueTimer);
     }
 
     void NextSentence()
     {
-        if (index <= sentences.Length - 1)
+        if (sentences.Count != 0)
         {
             dialogueText.text = "";
             StartCoroutine(WriteSentence());
@@ -59,8 +63,28 @@ public class DialogueController : MonoBehaviour
         {
             dialogueText.text = "";
             dialogueAnimator.SetTrigger("Exit");
-            index = 0;
-            startDialogue = true;
+        }
+    }
+
+    private IEnumerator animateCat()
+	{
+        while(true)
+		{
+            currentDialogBox.sprite = catClosed;
+            yield return new WaitForSeconds(animationDelay);
+            currentDialogBox.sprite = catOpened;
+            yield return new WaitForSeconds(animationDelay);
+        }
+	}
+
+    private IEnumerator animateGirl()
+    {
+        while (true)
+        {
+            currentDialogBox.sprite = girlClosed;
+            yield return new WaitForSeconds(animationDelay);
+            currentDialogBox.sprite = girlOpened;
+            yield return new WaitForSeconds(animationDelay);
         }
     }
 }
