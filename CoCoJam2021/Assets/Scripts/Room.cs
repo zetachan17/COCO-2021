4fa4699dc2a451c9f15b0be86407e7649b33c075
GJuteau <godefroy.juteau.1@ens.etsmtl.ca>
2021-11-14 00:42:23 -0500

    first floor

--- CoCoJam2021/Assets/Scripts/Room.cs
+++ CoCoJam2021/Assets/Scripts/Room.cs
@@ -39,7 +39,7 @@ public class Room : Interactable
     {
         DetectInteraction(KeyCode.E);
 
-        if (isPlayerInside)
+        if (isPlayerInside && door != null)
         {
             if (isPlayerInRange)
             {
